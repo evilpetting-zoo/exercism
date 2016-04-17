e58e2bcd16234896ea25f4564084f56f5dcb54be
Robert Koski <robert.koski1@gmail.com>
2016-04-16 20:35:57 -0500

    I was originally coding for user input, but it did not because it is an object with its own name attribute.

--- hello-world/hello_world.rb
+++ hello-world/hello_world.rb
@@ -1,15 +1,6 @@
 
 class HelloWorld
-	def self.hello
-		user_input = gets.chomp
-		if user_input == ""
-			name = nil
-		else name = user_input	
-		end
-		if name == nil
-			return "Hello, World!"
-		else
-			return "Hello #{name}! So nice to meet you!"
-		end
+	def self.hello(name = "World")
+		"Hello, #{name}!"
 	end
 end
